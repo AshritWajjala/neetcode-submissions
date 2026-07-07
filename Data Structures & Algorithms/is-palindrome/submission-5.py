d87539c8c1d5a887c73eaaa901e61d4021f69812
Ashrit Wajjala <ashritw2000@gmail.com>
2026-07-07 12:56:14 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            # To ignore non - alpha numeric characters
+            while l < r and not self.alpha_numeric(s[l]):
+                l += 1
+            while l < r and not self.alpha_numeric(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+
+        return True
+    # Used this approach to not use builtin method
+    def alpha_numeric(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
